e2040d5c5385280556f50ef47a8187a834d38f41
Tobias Bales <tobias.bales@gmail.com>
2020-07-09 10:47:53 +0200

    Disable helper generation for scaffolds

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module Pairing
 
     config.generators do |g|
       g.stylesheets false
+      g.helper = false
     end
 
     # Settings in config/environments/* take precedence over those specified here.
