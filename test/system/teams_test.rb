01ac6c927451cbf73814bd871c65b3dc0304def0
Tobias Bales <tobias.bales@gmail.com>
2020-07-10 14:05:33 +0200

    Fix system tests by adjusting to devise and ui changes

--- test/system/teams_test.rb
+++ test/system/teams_test.rb
@@ -5,41 +5,49 @@
 require 'application_system_test_case'
 
 class TeamsTest < ApplicationSystemTestCase
+  include Warden::Test::Helpers
+
   setup do
     @team = teams(:one)
+    login_as(users(:one), scope: :user)
   end
 
   test 'visiting the index' do
     visit teams_url
-    assert_selector 'h1', text: 'Teams'
+    assert_selector '.breadcrumb-item.active', text: 'Teams'
   end
 
   test 'creating a Team' do
     visit teams_url
-    click_on 'New Team'
+    click_on 'New team'
+    name = "new #{@team.name}"
 
-    fill_in 'Name', with: @team.name
-    click_on 'Create Team'
+    fill_in 'Name', with: name
+    click_on 'Create'
 
-    assert_text 'Team was successfully created'
+    assert_text "Saved #{name}"
     click_on 'Back'
   end
 
   test 'updating a Team' do
     visit teams_url
+    click_on 'TeamOne', match: :first
     click_on 'Edit', match: :first
 
-    fill_in 'Name', with: @team.name
-    click_on 'Update Team'
+    new_name = "#{@team.name} - updated"
+    fill_in 'Name', with: new_name
+    click_on 'Save'
 
-    assert_text 'Team was successfully updated'
+    assert_text "Saved #{new_name}"
     click_on 'Back'
   end
 
   test 'destroying a Team' do
     visit teams_url
+    click_on 'TeamOne', match: :first
+
     page.accept_confirm do
-      click_on 'Destroy', match: :first
+      click_on 'Delete', match: :first
     end
 
     assert_text 'Team was successfully destroyed'
