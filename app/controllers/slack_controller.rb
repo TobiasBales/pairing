34ae9152b96d9fbb317d409634cd4e6b9a1c903e
Tobias Bales <tobias.bales@gmail.com>
2020-07-19 16:47:35 +0200

    Allow to track pairing via slack
    closes #21

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -6,6 +6,7 @@ class SlackController < ApplicationController
 
   def track_pairing
     TrackPairingCommand.new(
+      message: T.cast(params[:text], String),
       slack_id: T.cast(params[:user_id], String),
       response_url: T.cast(params[:response_url], String)
     ).execute
--- test/controllers/slack_controller_test.rb
+++ test/controllers/slack_controller_test.rb
@@ -5,10 +5,16 @@ require 'test_helper'
 
 class SlackControllerTest < ActionDispatch::IntegrationTest
   test 'tracking pairing' do
-    post track_pairing_url, as: :json, params: {
-      user_id: users(:one).slack_accounts.first.slack_id,
-      response_url: ''
-    }
-    assert_response :ok
+    slack_client = Minitest::Mock.new
+    def slack_client.send_message(_url, _text); end
+
+    SlackClient.stub :new, slack_client do
+      post track_pairing_url, as: :json, params: {
+        user_id: users(:one).slack_accounts.first.slack_id,
+        text: "with @#{users(:two).slack_accounts.first.slack_name}",
+        response_url: ''
+      }
+      assert_response :ok
+    end
   end
 end
